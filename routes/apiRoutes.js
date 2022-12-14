09250652beea9f8aba2bbce51c9d9a173a1de2c1
Santhosh K Ambekar <44439022+ambekar92@users.noreply.github.com>
2022-12-14 11:45:41 +0530

    Updated apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -40,7 +40,7 @@ router.post("/api/logout", auth.authenticate, userObj.logout);
 
 router.post("/api/register_user", userObj.registerUser);
 
-router.get("/api/getUsers",auth.authenticate,  userObj.getUsers);
+router.get("/api/getUsers",  userObj.getUsers);
 
 router.post("/api/deleteUser", auth.authenticate, userObj.deleteUser);
 
