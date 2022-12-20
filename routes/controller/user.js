9fee7d806a2fcbfb5de6b890135fce7bb108564f
Santhosh K Ambekar <44439022+ambekar92@users.noreply.github.com>
2022-12-20 12:25:41 +0530

    SMS keys modified

--- routes/controller/user.js
+++ routes/controller/user.js
@@ -320,7 +320,7 @@ routes.prototype.saveSMSData = async function(req, res) {
         const options = { upsert: true };
         if(query.length > 0){
             for(let i=0; i<query.length; i++){
-                query[i] = _.pick(query[i],['address','appPhoneNumber','body','buildId','creator','date','date_sent','phoneData','error_code','priority','read','seen'])
+                query[i] = _.pick(query[i],['address','appPhoneNumber','body','buildId','creator','date','date_sent','phoneData'])
                 await userImplObj.saveSMSData(query[i],options);
             }
             responseObject.message = "Saved SMS Data in DB";
